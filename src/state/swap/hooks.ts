2df269ea25bfac70c20185b57ab25ef611fada93
Henk ter Harmsel <henkterharmsel92@gmail.com>
2021-01-30 00:34:23 +0100

    Added latest router

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -86,9 +86,8 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
-  '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // v2 router 02
+  '0x0EAb86c2532C200078358065c461e6C88C03013d', // factory
+  '0x466ff2466D08aB2a265b131F0d16fB9685ecC666' // router
 ]
 
 /**
